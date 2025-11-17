c7cf7d73018a75a3b4d238519c14b8a35c8c3a2a
TalAviaBiton <154813976+TalAviaBiton@users.noreply.github.com>
2025-11-17 12:48:51 +0200

    Add files via upload

---
+++ day2/TalBiton_print_chars.c
@@ -0,0 +1,32 @@
+/*------------------------------------------------------
+* Filename: TalBiton_print_chars.c
+* Description: recives a number and prints a square of a chracter according to ints deviders
+* Author: Tal Aviya Biton
+-------------------------------------------------------*/
+
+#include <stdio.h>
+
+int main() {
+    int num=0, i=0, j=0;
+    char to_print='@';
+    scanf("%d",&num);
+
+    if(num%2==0){
+        to_print='*';
+    }
+    else if(num%3==0){
+        to_print='^';
+    }
+    else if(num%5==0){
+        to_print='%';
+    }
+
+    for(i=0;i<num;i++){
+        for(j=0;j<num;j++){
+            printf("%c",to_print);
+        }       
+        printf("\n");
+    }
+    
+    return 0;
+}
